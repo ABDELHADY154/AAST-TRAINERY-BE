51e9d4ccabf74413f3364b8e76ca02395729907b
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-11-08 14:19:15 +0200

    model modify

--- app/Student.php
+++ app/Student.php
@@ -13,46 +13,6 @@ class Student extends User
 {
     use HasApiTokens, Notifiable, SoftDeletes;
     protected $fillable = [
-        'name', 'email', 'password', 'reg_no', 'period', 'gpa', 'college_id', 'image', 'start_year', 'end_year'
+        'name', 'email', 'password', 'reg_no', 'period', 'gpa', 'image', 'start_year', 'end_year'
     ];
-
-
-    // public function reviews()
-    // {
-    //     return $this->hasMany(Review::class);
-    // }
-
-    // public function college()
-    // {
-    //     return $this->belongsTo(College::class);
-    // }
-
-    // public function department()
-    // {
-    //     return $this->belongsTo(Department::class, 'department_id');
-    // }
-    // public function workExperience()
-    // {
-    //     return $this->hasMany(WorkExperience::class);
-    // }
-
-    // public function profileCourses()
-    // {
-    //     return $this->hasMany(ProfileCourse::class);
-    // }
-
-    // public function skills()
-    // {
-    //     return $this->hasMany(StudentSkill::class);
-    // }
-
-    // public function langs()
-    // {
-    //     return $this->hasMany(StudentLanguage::class);
-    // }
-
-    // public function accounts()
-    // {
-    //     return $this->hasOne(ProfileAccounts::class);
-    // }
 }
