77110464258628a4858ba8f09531803958337008
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-11-29 21:26:31 +0200

    .

--- database/seeds/StudentDepartmentSeeder.php
+++ database/seeds/StudentDepartmentSeeder.php
@@ -13,33 +13,33 @@ class StudentDepartmentSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::table('student_departments')->truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        // DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        // DB::table('student_departments')->truncate();
+        // DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         StudentDepartment::create([
             'department_name' => 'Business Information Systems'
         ]);
-        // StudentDepartment::create([
-        //     'department_name' => 'Marketing'
-        // ]);
-        // StudentDepartment::create([
-        //     'department_name' => 'Media Management'
-        // ]);
-        // StudentDepartment::create([
-        //     'department_name' => 'Accounting & Finance'
-        // ]);
-        // StudentDepartment::create([
-        //     'department_name' => 'Political Science'
-        // ]);
-        // StudentDepartment::create([
-        //     'department_name' => 'Media'
-        // ]);
-        // StudentDepartment::create([
-        //     'department_name' => 'Language and translation'
-        // ]);
-        // StudentDepartment::create([
-        //     'department_name' => 'Humanities'
-        // ]);
+        StudentDepartment::create([
+            'department_name' => 'Marketing'
+        ]);
+        StudentDepartment::create([
+            'department_name' => 'Media Management'
+        ]);
+        StudentDepartment::create([
+            'department_name' => 'Accounting & Finance'
+        ]);
+        StudentDepartment::create([
+            'department_name' => 'Political Science'
+        ]);
+        StudentDepartment::create([
+            'department_name' => 'Media'
+        ]);
+        StudentDepartment::create([
+            'department_name' => 'Language and translation'
+        ]);
+        StudentDepartment::create([
+            'department_name' => 'Humanities'
+        ]);
     }
 }
