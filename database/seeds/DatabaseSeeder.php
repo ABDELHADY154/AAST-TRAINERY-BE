ba219b496db987569e07f6686e4385b0e9a3c061
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-11-29 20:55:00 +0200

    .

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(StudentSeeder::class);
         $this->call(StudentDepartmentSeeder::class);
+
+        $this->call(StudentSeeder::class);
     }
 }
