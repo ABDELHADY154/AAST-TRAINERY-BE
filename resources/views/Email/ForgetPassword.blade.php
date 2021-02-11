ae5dae944e4271e467b6219bc432db3a8c14330d
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2021-02-11 23:29:56 +0200

    forget password validation

--- resources/views/Email/ForgetPassword.blade.php
+++ resources/views/Email/ForgetPassword.blade.php
@@ -1,43 +1,66 @@
-To reset your password Please <a href="{{config('app.url')}}/student/reset/{{$token}}">Click here</a>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        * {
+            box-sizing: border-box;
+        }
 
-{{-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            color: black;
+        }
+
+        /* Float four columns side by side */
+        .column {
+            float: left;
+            width: 90%;
+            padding: 0 10px;
+        }
+
+        /* Remove extra left and right margins, due to padding */
+        .row {
+            margin: 0 -5px;
+        }
+
+        /* Clear floats after the columns */
+        .row:after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+
+        /* Responsive columns */
+        @media screen and (max-width: 600px) {
+            .column {
+                width: 100%;
+                display: block;
+                margin-bottom: 20px;
+            }
+        }
+
+        /* Style the counter cards */
+        .card {
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            padding: 16px;
+            text-align: center;
+            background-color: white;
+        }
+
+    </style>
+</head>
+<body>
+
+    <div class="row">
+        <div class="column">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-<div class="card-body">
-    <form method="POST" action="{{ config('app.url')}}/api/W/reset">
-        @csrf
-
-        <input type="hidden" name="token" value="{{ $token }}">
-        <div class="form-group row">
-            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-            <div class="col-md-6">
-                <input id="password" type="password" class="form-control" name="password" required autocomplete="new-password">
-
+                <h2>AAST TRAINERY</h2>
+                <h4>
+                    To reset your password Please <a href="{{config('app.url')}}/student/reset/{{$token}}">Click here</a>
+                </h4>
             </div>
         </div>
-
-        <div class="form-group row">
-            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-            <div class="col-md-6">
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-            </div>
-        </div>
-
-        <div class="form-group row mb-0">
-            <div class="col-md-6 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Reset Password') }}
-                </button>
-            </div>
-        </div>
-    </form>
-</div>
-</div>
-</div>
-</div>
-</div> --}}
+    </div>
+</body>
+</html>
